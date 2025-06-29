dd476e45796ee1e59a660d2600ea537e0182fc7c
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2025-06-29 22:07:19 +1000

    feat: new socials

--- src/site.config.ts
+++ src/site.config.ts
@@ -7,7 +7,7 @@ export const theme: ThemeUserConfig = {
   /** Will be used in index page & copyright declaration */
   author: 'SethBurkart123',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Stay hungry, stay foolish',
+  description: 'My little space on the internet',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** Specify the default language for this site. */
